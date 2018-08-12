825dbea4bceed1098e7b765930fe0030b998f9b6
bashirsk <user.email>
2018-08-12 13:58:20 +0100

    Initial Commit

---
+++ GithubCommits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubCommits
+//
+//  Created by Bashir Sentongo on 12/08/2018.
+//  Copyright © 2018 Bashir Sentongo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
