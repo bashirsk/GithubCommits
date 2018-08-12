c03994df141eee6e05bf4836a705162a67528723
bashirsk <user.email>
2018-08-12 15:28:22 +0100

    Created API Client Created models to store our data

---
+++ GithubCommits/Models/AuthorInfo.swift
@@ -0,0 +1,15 @@
+//
+//  CommitInformation.swift
+//  GithubCommits
+//
+//  Created by Bashir Sentongo on 12/08/2018.
+//  Copyright © 2018 Bashir Sentongo. All rights reserved.
+//
+
+import Foundation
+
+struct AuthorInfo: Codable {
+    let name: String
+    let date: Date
+    let email: String
+}
