651c70e4d7da961d8e48b57b3465ec3db82daf0f
bashirsk <user.email>
2018-08-12 15:29:36 +0100

    Added root model

---
+++ GithubCommits/Models/Root.swift
@@ -0,0 +1,14 @@
+//
+//  Root.swift
+//  GithubCommits
+//
+//  Created by Bashir Sentongo on 12/08/2018.
+//  Copyright © 2018 Bashir Sentongo. All rights reserved.
+//
+
+import Foundation
+
+struct Root {
+    let commit: AuthorInfo
+    let author: AuthorImage
+}
